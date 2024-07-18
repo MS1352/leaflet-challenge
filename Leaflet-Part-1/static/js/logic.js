9931c7605b4c896e078e0785e3c747afc4cd7a48
MS1352 <mason.seifaddini@gmail.com>
2024-07-18 23:47:14 +0800

    applying changes

---
+++ Leaflet-Part-1/static/js/logic.js
@@ -0,0 +1,71 @@
+// Initialize the map
+var map = L.map('map').setView([0, 0], 2);
+
+// Add a tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Fetch earthquake data using D3
+d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson').then(data => {
+    // Function to determine marker size based on magnitude
+    function markerSize(magnitude) {
+        return magnitude * 4;
+    }
+
+    // Function to determine marker color based on depth
+    function markerColor(depth) {
+        if (depth > 90) return "#ff5f65";
+        else if (depth > 70) return "#fca35d";
+        else if (depth > 50) return "#fdb72a";
+        else if (depth > 30) return "#f7db11";
+        else if (depth > 10) return "#dcf400";
+        else return "#a3f600";
+    }
+
+    // Add GeoJSON layer to the map
+    L.geoJSON(data, {
+        pointToLayer: function(feature, latlng) {
+            return L.circleMarker(latlng, {
+                radius: markerSize(feature.properties.mag),
+                fillColor: markerColor(feature.geometry.coordinates[2]),
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            });
+        },
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>Magnitude: ${feature.properties.mag}</p><p>Depth: ${feature.geometry.coordinates[2]}</p>`);
+        }
+    }).addTo(map);
+});
+
+// Create a legend control object
+var legend = L.control({position: 'bottomright'});
+
+// Function to generate the legend content
+legend.onAdd = function(map) {
+    var div = L.DomUtil.create('div', 'info legend');
+    var depths = [-10, 10, 30, 50, 70, 90];
+    var colors = [
+        "#a3f600",
+        "#dcf400",
+        "#f7db11",
+        "#fdb72a",
+        "#fca35d",
+        "#ff5f65"
+    ];
+
+    // Loop through depth intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < depths.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + colors[i] + '"></i> ' +
+            depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+// Add the legend to the map
+legend.addTo(map);
